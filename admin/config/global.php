b3e8315c406ca74517f8c3312a3baa51dde41870
iTzprodigui <carlos_emil_08@hotmail.com>
2022-03-07 20:54:22 -0600

    Bd

--- admin/config/global.php
+++ admin/config/global.php
@@ -1,17 +1,17 @@
 <?php 
 //ip de la pc servidor base de datos
-define("DB_HOST", "localhost");
+define("DB_HOST", "us-cdbr-east-05.cleardb.net");
 
 // nombre de la base de datos
-define("DB_NAME", "control_asistencia");
+define("DB_NAME", "heroku_a1235d3cd696b69");
 
 
 //nombre de usuario de base de datos
-define("DB_USERNAME", "root");
+define("DB_USERNAME", "bc2c1821dde691");
 //define("DB_USERNAME", "u222417_admin");
 
 //conraseña del usuario de base de datos
-define("DB_PASSWORD", "");
+define("DB_PASSWORD", "391acc9d");
 //define("DB_PASSWORD", "Enero2020Admin");
 
 //codificacion de caracteres
